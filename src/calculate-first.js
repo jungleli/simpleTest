33b82f78174fc09864e915261235fc53a73ec577
jungleli <jungle216@126.com>
2015-11-24 18:20:48 +0800

    refacting

--- src/calculate-first.js
+++ src/calculate-first.js
@@ -1,24 +1,5 @@
 function single_element(arr) {
-    var tempArr = [];
-
-    for(var i=0; i<arr.length; i++){
-        if(i%2==1){
-            tempArr.push(arr[i]);
-        }
-    }
-    var result = [];
-    for(var j=0; j<tempArr.length;j++){
-        var index = result.indexOf(tempArr[j]);
-        if(index == -1){
-            result.push(tempArr[j]);
-        }
-        else {
-            result.splice(index, 1);
-        }
-        }
-    if(result.length==1){
-        result.shift();
-    }
-    console.log(result);
-    return result;
+    return arr
+            .filter((val, key) => key % 2 == 1)
+            .filter((val, key, newArr) => newArr.indexOf(val) == newArr.lastIndexOf(val));
 }
--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,18 +1,14 @@
 function calculate_median(arr) {
-    var tempArr = [];
-
-    for(var i=0; i<arr.length; i++){
-        if(i%2==1){
-            tempArr.push(arr[i]);
-        }
-    }
+    var tempArr = arr
+            .filter((val, key) => key % 2 == 1)
+            .sort((a, b) => a - b);
 
     var len = tempArr.length;
 
-    if(len%2 ==1){
-        return tempArr[(len-1)/2];
+    if (len % 2 == 1) {
+        return tempArr[(len - 1) / 2];
     }
-    else{
-        return (tempArr[len/2-1] + tempArr[len/2])/2;
+    else {
+        return (tempArr[len / 2 - 1] + tempArr[len / 2]) / 2;
     }
 }
