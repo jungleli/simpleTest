c4a03405fa655c221e7fbf24260301a0c013cd4e
jungleli <jungle216@126.com>
2015-11-24 17:48:19 +0800

    src: first two method

--- src/calculate-second.js
+++ src/calculate-second.js
@@ -1,3 +1,18 @@
 function calculate_median(arr) {
+    var tempArr = [];
 
+    for(var i=0; i<arr.length; i++){
+        if(i%2==1){
+            tempArr.push(arr[i]);
+        }
+    }
+
+    var len = tempArr.length;
+
+    if(len%2 ==1){
+        return tempArr[(len-1)/2];
+    }
+    else{
+        return (tempArr[len/2-1] + tempArr[len/2])/2;
+    }
 }
